8b470a63da1f8920b77eb30872b85ef948aa7b8e
Raymond <raymondjohnson@pursuit.org>
2024-07-08 08:50:46 -0400

    feat: create route to post new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,11 +25,11 @@ userController.get('/:id', async (req, res) => {
 })
 // * * CREATE USER
 userController.post('/', async (req, res) => {
-  const body = req.body
-  const id = req.params.id
+  const {fname, lname, city, website, created_at} = req.body
+  // const id = req.params.id
   
   try {
-    const newUser = await db.one('INSERT INTO users03 (fname, lname, city, website, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING id', [fname, lname, city, website, created_at]);
+    const newUser = await db.one('INSERT INTO users03 (fname, lname, city, website, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING *', [fname, lname, city, website, created_at]);
        res.status(201).json(newUser)
   } catch (error){
     res.status(400).json({error: 'Server not responding'})
