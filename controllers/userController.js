0e2b0d5ab232d1e478e11911d67ff1a4455ce654
Raymond <raymondjohnson@pursuit.org>
2024-07-20 09:29:51 -0400

    query updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,15 +3,6 @@ const userController = express.Router()
 const db = require('../db/dbConfig.js')
 
 // * * * *  ROUTES * * * *
-// * * ALL USERS
-userController.get('/', async (req, res) => {
-  try {
-    const users = await db.any('SELECT * FROM users03')
-    res.status(200).json(users)
-  } catch (error){
-    res.status(400).json({error: 'Server not responding'})
-  }
-})
 // * * SINGLE USER
 userController.get('/:id', async (req, res) => {
   // const id = req.params.id
@@ -19,6 +10,15 @@ userController.get('/:id', async (req, res) => {
   try {
     const user = await db.any('SELECT * FROM users03 WHERE id=$1',[id])
     res.status(200).json(user)
+  } catch (error){
+    res.status(400).json({error: 'Server not responding'})
+  }
+})
+// * * ALL USERS
+userController.get('/', async (req, res) => {
+  try {
+    const users = await db.any('SELECT * FROM users03')
+    res.status(200).json(users)
   } catch (error){
     res.status(400).json({error: 'Server not responding'})
   }
